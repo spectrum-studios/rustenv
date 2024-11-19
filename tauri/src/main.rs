ee5ab33685bf0efe9411b25a6fe964220b33a084
Nicholas Ng <onenylxus@gmail.com>
2024-11-19 17:49:15 +0000

    Support display forwarding from container

--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -1,7 +1,13 @@
 #![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
+use rustenv_backend::app;
+
 fn main() {
+    let port = portpicker::pick_unused_port().expect("failed to pick port");
+    tauri::async_runtime::spawn(app(port));
+
     tauri::Builder::default()
+        .manage(port)
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
